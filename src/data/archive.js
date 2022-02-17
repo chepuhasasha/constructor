c68d4b91d478686184e6fde3e5ea0159d72d9600
Sergey Batukov <sashachepuha@gmail.com>
2022-02-17 21:38:45 +0300

    add api scheme

--- src/data/archive.js
+++ src/data/archive.js
@@ -5,6 +5,7 @@ export default [
     userID: 1,
     timestamp: 123223,
     code: "",
+    commit: "",
   },
   {
     ID: 2,
@@ -12,5 +13,6 @@ export default [
     userID: 1,
     timestamp: 123223,
     code: "",
+    commit: "",
   },
 ];
