dbbaac6ef9fc26ce137d3f81bba4c39b81333c15
Sergey Batukov <sashachepuha@gmail.com>
2022-02-22 10:32:46 +0000

    update

--- backend/parser.js
+++ backend/parser.js
@@ -1,7 +1,18 @@
 export default {
+  map: {
+    'VERTICAL': 'column',
+    'HORIZONTAL': 'row',
+  },
+  rgba({ r = 1, g = 1, b = 1, a = 1 }) {
+    return `rgba(${r * 255}, ${g * 255}, ${b * 255}, ${a})`;
+  },
+  // TODO
   parse(obj) {
     const result = {
       childs: [],
+      style: {
+        background: this.background(obg.background)
+      }
     };
     obj.children.forEach((child) => {
       result.childs.push(this.parse(child));
@@ -9,7 +20,19 @@ export default {
     return result;
   },
 
-  background({ r = 1, g = 1, b = 1, a = 1 }) {
-    return `rgba(${r * 255}, ${g * 255}, ${b * 255}, ${a})`;
+  // TODO
+  background(bg = []) {
+    const result = ''
+    bg.forEach((item) => {
+      switch (item.type) {
+        case 'SOLID':
+          result = this.rgba(item.color)
+          break;
+      
+        default:
+          break;
+      }
+    });
+    return result;
   },
 };
